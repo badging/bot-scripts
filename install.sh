9ec98fa256121f8386d7d14311e08d7f462c7d2d
Kaxada <ekaxada@gmail.com>
2023-10-04 13:24:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ while true; do
         echo
         # Setup Githubcli Keyring
         if type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-        && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-        && curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - 
-        && sudo apt update && sudo apt install -y gh git nodejs; then
+        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && \
+        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && \
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && \
+        curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && \
+        sudo apt update && sudo apt install -y gh git nodejs; then
             echo
             break
         else
